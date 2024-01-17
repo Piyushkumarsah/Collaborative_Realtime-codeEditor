7c999ab33ae2d37fb5214b9bc7cb125094737aa5
Piyush Kumar <piyushk.ee.21@nitj.ac.in>
2024-01-17 18:17:20 +0530

    ioURLU

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -7,7 +7,7 @@ import { io } from 'socket.io-client'
 import { useLocation, useNavigate, Navigate } from 'react-router';
 import toast from 'react-hot-toast';
 
-const socket = io('http://localhost:5000', {
+const socket = io('https://realtimeeditor-2r9t.onrender.com:8080', {
     reconnectionAttempts: Infinity,
     transports: ['websocket']
 })
